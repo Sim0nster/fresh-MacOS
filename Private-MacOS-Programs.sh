ba77cbad23e081bcb348d5a89de9984e7c74f645
Sim0nster <88711572+Sim0nster@users.noreply.github.com>
2023-10-31 12:14:46 +0100

    Create Private-MacOS-Programs.sh

---
+++ Private-MacOS-Programs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+brew install \
+  adobe-acrobat-reader \
+  alt-tab \
+  arc-browser \
+  steam \
+  google-chrome \
+  iterm2 \
+  macpass \
+  microsoft-office \
+  powershell \
+  tunnelblick \
+  visual-studio-code
