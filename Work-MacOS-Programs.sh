c83c17077dcea51fd880dfee0bb29ffb3cef09d8
Sim0nster <88711572+Sim0nster@users.noreply.github.com>
2023-10-31 12:16:45 +0100

    Add files via upload

---
+++ Work-MacOS-Programs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+brew install \
+  adobe-acrobat-reader \
+  alt-tab \
+  anydesk \
+  cyberduck \
+  firefox \
+  gimp \
+  google-chrome \
+  inkscape \
+  iterm2 \
+  macpass \
+  microsoft-edge \
+  microsoft-office \
+  microsoft-teams \
+  powershell \
+  remote-desktop-manager \
+  slack \
+  stats \
+  teamviewer \
+  tunnelblick \
+  visual-studio-code
